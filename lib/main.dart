08277b52a7f21b1d8898df4eae6d1b600c7d32fc
Ivan Trj <ivantrajanovski@gmail.com>
2022-07-02 13:20:09 +0200

    change design

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Color.fromRGBO(41, 40, 40, 1),
       ),
       home: const MyHomePage(),
     );
@@ -54,12 +55,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Timer App"),
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             (_counter > 0)
                 ? Text("")
@@ -73,10 +71,18 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
             Text(
               '$_counter',
-              style: TextStyle(fontSize: 30),
+              style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
             ElevatedButton(
               onPressed: () => _startTimer(),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                shape: CircleBorder(),
+                fixedSize: Size(110, 110),
+              ),
               child: Text("Start timer"),
             ),
           ],
